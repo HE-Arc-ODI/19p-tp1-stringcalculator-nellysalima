762b62a733f69b63257ccf7b285e59531f6fc9e7
nellysalima <nelly.jaquet@he-arc.ch>
2019-02-18 21:31:49 +0100

    Initialisation

---
+++ src/main/java/StringCalculator.java
@@ -0,0 +1,24 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author nellysal.jaquet
+ */
+public class StringCalculator {
+    
+    //constructeur prenant un chiffre en paramètre
+    public StringCalculator(int nb1) {}
+ 
+    
+    int Add(String numbers){
+    
+    return numbers;
+    }
+    
+    
+  
+}
