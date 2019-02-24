7ef0381e4bbe22944d7aa8b73b15edfe6a0d3e76
nellysalima <nelly.jaquet@he-arc.ch>
2019-02-24 19:48:11 +0100

    improve code method add

--- src/test/java/StringCalculatorTest.java
+++ src/test/java/StringCalculatorTest.java
@@ -54,6 +54,21 @@ public class StringCalculatorTest {
    
   }
 
+  
+   @Test
+  public void testAdd2() {
+    // Arrange
+    StringCalculator sc = new StringCalculator(0);
+    String numbers = "1,2";    
+    
+    // Act
+    int expected;
+    expected = sc.add(numbers);
+    
+    // Assert
+    assertThat(expected, is(1));
+   
+  }
 
 
 }
